769fa16ae6bc4796437f1f8c93f54196f8e464c6
Tim <camp4climber@gmail.com>
2017-01-04 14:26:30 -0700

    Alias post in page_controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule Mtnblog.PageController do
   use Mtnblog.Web, :controller
 
+  alias Mtnblog.Post
+
   def index(conn, _params) do
     posts = Repo.all(Post)
     render(conn, "index.html", posts: posts)
