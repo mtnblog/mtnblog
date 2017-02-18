f65aec19eb637162b09cc8be26366a244afb0163
Tim <camp4climber@gmail.com>
2017-02-17 17:39:49 -0700

    Add existence check for masonry grid

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,15 +1,16 @@
-import "imagesloaded"
 import fitvids from "fitvids"
+import "imagesloaded"
 import Masonry from "masonry-layout"
 
-fitvids();
-
-$('.grid').imagesLoaded(function() {
-  $('.grid').animate({'opacity': 1});
-  var masonry = new Masonry('.grid', {
-    columnWidth: '.grid-sizer',
-    itemSelector: '.grid-item',
-    gutter: 5 
+if ($('.grid').length) {
+  $('.grid').imagesLoaded(function() {
+    $('.grid').animate({'opacity': 1});
+    var masonry = new Masonry('.grid', {
+      columnWidth: '.grid-sizer',
+      itemSelector: '.grid-item',
+      gutter: 5 
+    });
   });
-});
+}
 
+fitvids();
