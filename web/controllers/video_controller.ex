8918a63367c6942b9ffb09b33d22b614fd023458
Tim <camp4climber@gmail.com>
2017-03-26 15:25:54 -0600

    Fix video controller bug

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -10,7 +10,8 @@ defmodule Mtnblog.VideoController do
 
   def show(conn, %{"id" => id}) do
     query = from v in Video, order_by: [desc: v.inserted_at]    
-    [video | videos] = Repo.all(query)
+    video = Repo.get!(Video, id)
+    videos = Repo.all(query)
     render(conn, "show.html", video: video, videos: videos)
   end
 end
